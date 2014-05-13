407051d55e32f36188a074eb9d36d9214113cab5
Arlen <arlen@AELIUS.(none)>
2014-05-13 17:04:15 +0200

    Kommentering

--- MainPanel.java
+++ MainPanel.java
@@ -1,14 +1,16 @@
-package Boligformidling;
+package boligformidling;
+
 import java.awt.GridLayout;
-
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-
 
 public abstract class MainPanel extends JPanel{
     private Vindu_Main parent;
     private Bruker bruker;
-    
+    /**
+     * MainPanel opptar hele Vindu_Main og har som hovedfunksjon å hoste SubPanel'er, men kan også brukes til annet.
+     * @param parent
+     * @param bruker 
+     */
     public MainPanel(Vindu_Main parent,Bruker bruker){
     	super(new GridLayout(0,1));
         this.parent = parent;
