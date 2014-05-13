f7f34c2a2476419f0bf28de3367c2ad1108008a5
Alex-Gaard <alex.gaard@hotmail.no>
2014-05-13 16:47:22 +0200

    Update MainPanel.java

--- MainPanel.java
+++ MainPanel.java
@@ -1,30 +1,25 @@
-package boligformidling;
+package Boligformidling;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-/**
- * MainPanel'er har i oppgave å 'hoste' SubPanel'er.
- * @author Arlen Syver Wasserman, s193956, IT 1 år
- * @version 1.0 13 Mai 2014
- */
+
 public abstract class MainPanel extends JPanel{
     private Vindu_Main parent;
     private Bruker bruker;
-    JTabbedPane tabPane;
-    /**
-     * Konstruktør som tar imot et Vindu_Main og en bruker
-     * @param Parent Denne sendes med for å kunne utføre operasjoner på den.
-     * @param Bruker Holder brukeren som benytter seg av funksjonene
-     */
-    public MainPanel(Vindu_Main Parent, Bruker Bruker) {
-        parent = Parent;
-        bruker = Bruker;
+    
+    public MainPanel(Vindu_Main parent,Bruker bruker){
+    	super(new GridLayout(0,1));
+        this.parent = parent;
+        this.bruker = bruker;
     }
-    Vindu_Main getRoot() {
-        return parent;
-    }
-    Bruker getBruker() {
+    
+   public Bruker getBruker(){
         return bruker;
     }
-}//End class MainPanel
+    
+   public Vindu_Main getRoot(){
+        return parent;
+    }
+}
