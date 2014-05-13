e1fbcff1b58cd1e1237f0ac30cf75bb51a07ca0d
Alex-Gaard <alex.gaard@hotmail.no>
2014-05-13 16:02:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 package Boligformidling;
 
+/**
+ * Klassen har ansvar for å starte opp applikasjonen.
+ * 
+ * @author Alexander Gård, s198585, 1. år IT
+ * @version 1.00, 12 Mai 2014
+ */
 public class Main {
-
-	public static void main(String[] args) throws Exception {
+	/**
+	 * Starter opp applikasjonen.
+	 */
+	public static void main(String[] args) {
 
 		new Vindu_Main();
-				  
-	}//end of main
 
-}//end of class Main
+	}// end of main
+
+}// end of class Main
